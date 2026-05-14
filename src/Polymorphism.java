ba1a198745850b5694d8438dceaecf6ccdc60f62
Bryan Kim <bryankim@Bryans-MacBook-Air-4.local>
2026-05-14 18:59:08 -0400

    fixed bug

--- src/Polymorphism.java
+++ src/Polymorphism.java
@@ -8,6 +8,10 @@ public class Polymorphism {
      */
     public static String getStudentRole(Person p) {
         // TODO: right now for student, it is not returning the role to be student.
+
+        if (p instanceof Student s) {
+            return s.role;
+        }
         return p.role;
     }
 }
